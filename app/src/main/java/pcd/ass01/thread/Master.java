c0c408e9409b23f5be1bc68841c7b9ecc4c70a7e
Oldranda <leonardorandacio@gmail.com>
2024-03-26 18:43:01 +0100

    continued master

--- app/src/main/java/pcd/ass01/thread/Master.java
+++ app/src/main/java/pcd/ass01/thread/Master.java
@@ -1,12 +1,40 @@
 package pcd.ass01.thread;
 
-public class Master extends Thread{
+import java.util.concurrent.Phaser;
 
-    private Latch latch;
+public abstract class Master extends Thread{
+
+    private Phaser workDoneLatch;
+    private Phaser startWorkLatch;
     private int nWorkers;
-    
-    public void run(){
+    private int nAgents;
+    private AbstractEnvironment env;
 
+    public Master(final int nWorkers,
+            final int nAgents,
+            final AbstractEnvironment env
+            ){
+        this.nWorkers = nWorkers;
+        this.nAgents = nAgents;
+        this.workDoneLatch = new Phaser(nWorkers);
+        this.startWorkLatch = new Phaser(1);
+        this.env = env;
+
+    }
+    
+    public void run(int nSteps){
+        log("Starting Simulation");
+        Worker[] workers = new Worker[this.nWorkers];
+        for(var worker : workers){
+            worker = new Worker(this.workDoneLatch, this.startWorkLatch);
+        }
+        for(int step = 1; step <= nSteps; step++){
+            this.env.step();
+            //TODO ADD SENSE-DECIDE TASKS TO THE BAG OF WORKS
+            this.startWorkLatch.(); //notifing workers that bag is full of work
+            this.workDoneLatch.await(); //wait for all workers to finish the works
+            //TODO ADD SENSE-DECIDE TASKS TO THE BAG OF WORKS
+        }
     }
     
     private void log(String message){
@@ -14,4 +42,8 @@ public class Master extends Thread{
             System.out.println("[master]: " + message);
         }
     }
+
+    protected abstract void senseDecide();
+
+    protected abstract void act();
 }
