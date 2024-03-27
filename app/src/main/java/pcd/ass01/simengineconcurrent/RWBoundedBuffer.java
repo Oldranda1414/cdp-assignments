02cceaf50bd6ed8dba10f3564ba78b31431f0b77
Oldranda <leonardorandacio@gmail.com>
2024-03-27 18:27:28 +0100

    implemented RWBoundedBuffer

--- app/src/main/java/pcd/ass01/simengineconcurrent/RWBoundedBuffer.java
+++ app/src/main/java/pcd/ass01/simengineconcurrent/RWBoundedBuffer.java
@@ -1,15 +1,37 @@
 package pcd.ass01.simengineconcurrent;
 
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+
+import javax.xml.crypto.Data;
+
 public abstract class RWBoundedBuffer<I> implements BoundedBuffer<I> {
 
-    
+   private final Map<String, I> m = new TreeMap<String, I>();
+   private final ReentrantReadWriteLock rwl = new ReentrantReadWriteLock();
+   private final Lock r = rwl.readLock();
+   private final Lock w = rwl.writeLock();
 
-    @Override
-    public void put(I item) throws InterruptedException {
-    }
-
-    @Override
-    public I get() throws InterruptedException {
-    }
-    
+   public I get(String key) {
+     r.lock();
+     try { return m.get(key); }
+     finally { r.unlock(); }
+   }
+   public String[] allKeys() {
+     r.lock();
+     try { return m.keySet().toArray(); }
+     finally { r.unlock(); }
+   }
+   public I put(String key, I value) {
+     w.lock();
+     try { return m.put(key, value); }
+     finally { w.unlock(); }
+   }
+   public void clear() {
+     w.lock();
+     try { m.clear(); }
+     finally { w.unlock(); }
+   } 
 }
