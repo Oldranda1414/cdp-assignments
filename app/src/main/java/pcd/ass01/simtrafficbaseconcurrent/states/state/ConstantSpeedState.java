b22b662ec3f3b290adce1e5d04494fdc100c518d
Oldranda <leonardorandacio@gmail.com>
2024-04-02 14:44:44 +0200

    improved AgentState while working on improving the method act

--- app/src/main/java/pcd/ass01/simtrafficbaseconcurrent/states/state/ConstantSpeedState.java
+++ app/src/main/java/pcd/ass01/simtrafficbaseconcurrent/states/state/ConstantSpeedState.java
@@ -1,13 +1,15 @@
 package pcd.ass01.simtrafficbaseconcurrent.states.state;
 
+import pcd.ass01.simengineconcurrent.AbstractEnvironment;
 import pcd.ass01.simengineconcurrent.AgentState;
+import pcd.ass01.simtrafficbaseconcurrent.agent.CarAgent;
 import pcd.ass01.simtrafficbaseconcurrent.environment.RoadsEnv;
 
-public class ConstantSpeedState implements AgentState<RoadsEnv>{
+public class ConstantSpeedState implements AgentState<CarAgent>{
 
     @Override
-    public void act(String id, RoadsEnv env) {
-        env.updateAgent(id, 0);
+    public void act(String id, AbstractEnvironment<CarAgent> env) {
+        ((RoadsEnv) env).updateAgent(id, 0);
     }
 
 }
