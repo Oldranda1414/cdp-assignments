488768c4bce4ff28927b4a6cff7f588bc9e73700
Oldranda <leonardorandacio@gmail.com>
2024-04-04 22:56:51 +0200

    implemented TrafficLight

--- app/src/main/java/pcd/ass01/simtrafficbaseconcurrent/entity/TrafficLight.java
+++ app/src/main/java/pcd/ass01/simtrafficbaseconcurrent/entity/TrafficLight.java
@@ -1,5 +1,77 @@
 package pcd.ass01.simtrafficbaseconcurrent.entity;
 
-public class TrafficLight {
-    
+import pcd.ass01.simtrafficbaseconcurrent.environment.Road;
+
+public class TrafficLight implements RoadEntity{
+	public static enum TrafficLightState {GREEN, YELLOW, RED}
+	private TrafficLightState state, initialState;
+	private int currentTimeInState;
+	private int redDuration, greenDuration, yellowDuration;
+	private double position;
+    private Road road;
+	
+	public TrafficLight(double pos, Road road, TrafficLightState initialState, int greenDuration, int yellowDuration, int redDuration) {
+		this.redDuration = redDuration;
+		this.greenDuration = greenDuration;
+		this.yellowDuration = yellowDuration;
+		this.position = pos;
+		this.initialState = initialState;
+        this.road = road;
+	}
+	
+	public void init() {
+		state = initialState;
+		currentTimeInState = 0;
+	}
+
+	public void step(int dt) {
+		switch (state) {
+		case TrafficLightState.GREEN: 
+			currentTimeInState += dt;
+			if (currentTimeInState >= greenDuration) {
+				state = TrafficLightState.YELLOW; 
+				currentTimeInState = 0;
+			}
+			break;
+		case TrafficLightState.RED: 
+			currentTimeInState += dt;
+			if (currentTimeInState >= redDuration) {
+				state = TrafficLightState.GREEN; 
+				currentTimeInState = 0;
+			}
+			break;
+		case TrafficLightState.YELLOW: 
+			currentTimeInState += dt;
+			if (currentTimeInState >= yellowDuration) {
+				state = TrafficLightState.RED; 
+				currentTimeInState = 0;
+			}
+			break;
+		default:
+			break;
+		}
+	}
+	
+	public boolean isGreen() {
+		return state.equals(TrafficLightState.GREEN);
+	}
+	
+	public boolean isRed() {
+		return state.equals(TrafficLightState.RED);
+	}
+
+	public boolean isYellow() {
+		return state.equals(TrafficLightState.YELLOW);
+	}
+	
+    @Override
+	public double getCurrentPosition() {
+		return position;
+	}
+
+    @Override
+    public Road getRoad() {
+        return road;
+    }
+
 }
