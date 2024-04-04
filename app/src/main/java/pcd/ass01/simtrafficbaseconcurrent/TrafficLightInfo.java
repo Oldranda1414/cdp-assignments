dd851fba430fde68fcb8196e85c7e772ca647f3b
Oldranda <leonardorandacio@gmail.com>
2024-04-04 23:42:57 +0200

    deleted old traffic light classes

--- app/src/main/java/pcd/ass01/simtrafficbaseconcurrent/TrafficLightInfo.java
+++
@@ -1,5 +0,0 @@
-package pcd.ass01.simtrafficbaseconcurrent;
-
-import pcd.ass01.simtrafficbaseconcurrent.environment.Road;
-
-public  record TrafficLightInfo(TrafficLight sem, Road road, double roadPos) {}
