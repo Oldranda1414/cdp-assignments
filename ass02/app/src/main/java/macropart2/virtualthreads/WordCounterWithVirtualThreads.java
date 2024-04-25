113cb9ab3517faf92f098e406318b13c66dff714
Oldranda1414 <leonardo.randacio@gmail.com>
2024-04-25 19:07:25 +0200

    added base vt class

---
+++ ass02/app/src/main/java/macropart2/virtualthreads/WordCounterWithVirtualThreads.java
@@ -0,0 +1,39 @@
+package macropart2.virtualthreads;
+
+import java.util.Map;
+
+import macropart2.WordCounter;
+
+public class WordCounterWithVirtualThreads implements WordCounter{
+
+    @Override
+    public Map<String, Integer> getWordOccurrences() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getWordOccurrences'");
+    }
+
+    @Override
+    public void pause() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'pause'");
+    }
+
+    @Override
+    public boolean isPaused() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'isPaused'");
+    }
+
+    @Override
+    public void resume() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'resume'");
+    }
+
+    @Override
+    public void join() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'join'");
+    }
+    
+}
