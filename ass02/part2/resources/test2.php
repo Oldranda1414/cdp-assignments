5e52b7e46de9ee1f1ff799840a99069a931c1eff
Oldranda1414 <leonardo.randacio@gmail.com>
2024-05-11 19:02:59 +0200

    added doc to the single parts

---
+++ ass02/part2/resources/test2.php
@@ -0,0 +1,12 @@
+<?php
+echo "<!DOCTYPE html>
+<html>
+    <head>
+        <title>Test 1</title>
+    </head>
+
+    <body>
+        <a href='http://192.168.1.152/Filo/test1.php'>Test link 1</a>
+    </body>
+</html>";
+?>
