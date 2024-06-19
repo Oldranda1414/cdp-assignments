545158c2879de7b1718f00c100e07251d5bcecde
FilippoGurioli <filippo.gurioli@studio.unibo.it>
2024-06-19 12:23:54 +0200

    Improved a bit UX

--- Assignment-03/part2A/src/public/js/View/view.js
+++ Assignment-03/part2A/src/public/js/View/view.js
@@ -5,18 +5,23 @@ class PreLobbyView extends BaseView {
     _initialize() {
         this._log("This view is " + this.viewId);
         userID.innerHTML = this.viewId;
-        this.displaySudoku();
+        this.setupSudoku();
     }
 
-    displaySudoku() {
+    setupSudoku() {
         for (let i = 0; i < 81; i++) {
             const cell = document.createElement('div');
             cell.className = 'sudokuCell';
             cell.contentEditable = true;
             cell.setAttribute('data-index', i);
-            cell.addEventListener('input', this.handleInput);
+            cell.addEventListener('keypress', this.handleKeyPress);
             sudokuContainer.appendChild(cell);
         }
+        sudokuContainer.hidden = true;
+    }
+
+    showSudoku() {
+        sudokuContainer.hidden = false;
     }
 
     handleInput() {
@@ -26,6 +31,16 @@ class PreLobbyView extends BaseView {
         if (!/^[1-9]$/.test(value)) {
             cell.textContent = '';
         }
+    }
+
+    handleKeyPress(e) {
+        const key = e.key;
+        const cell = e.target;
+        // Prevent default behavior if the key is not a number between 1 and 9
+        if (/^[1-9]$/.test(key) && key !== '0') {
+            cell.textContent = key;
+            cell.blur();
+        } else e.preventDefault();
     }
 
     _gameOver() {
