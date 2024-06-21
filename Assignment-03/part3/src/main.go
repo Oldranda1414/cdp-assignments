c8e361ba32af96667aea8644718af2c0894d70d6
Oldranda1414 <leonardo.randacio@gmail.com>
2024-06-21 18:29:47 +0200

    refactoring of code

--- Assignment-03/part3/src/main.go
+++ Assignment-03/part3/src/main.go
@@ -11,23 +11,17 @@ func main(){
     fmt.Println("Welcome to Guess the Number")
 
     fmt.Print("Enter the number of players: ")
-    if _, err := fmt.Scanln(&numberOfPlayers); err != nil {
-        fmt.Println("Invalid input. Please enter a valid integer.")
-        return
-    }
+	scanInteger(&numberOfPlayers)
 
-    fmt.Print("Enter the max value for the extracted number: ")
-    if _, err := fmt.Scanln(&maxValueForExtractedNumber); err != nil {
-        fmt.Println("Invalid input. Please enter a valid integer.")
-        return
-    }
+   	fmt.Print("Enter the max value for the extracted number: ")
+	scanInteger(&maxValueForExtractedNumber)
 
     fmt.Println("The number of players is :", numberOfPlayers)
     fmt.Println("The range of the extacted number is: 0 -", maxValueForExtractedNumber)
 }
 
-func scanInteger(variable int){
-    if _, err := fmt.Scanln(&variable); err != nil {
+func scanInteger(variable *int){
+    if _, err := fmt.Scanln(variable); err != nil {
         fmt.Println("Invalid input. Please enter a valid integer.")
         return
     }
