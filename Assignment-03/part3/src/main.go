4c7ad179b11da82f0765f807981963f0ac86cf48
Oldranda1414 <leonardo.randacio@gmail.com>
2024-06-21 18:56:27 +0200

    implemented simple goroutine and improved code

--- Assignment-03/part3/src/main.go
+++ Assignment-03/part3/src/main.go
@@ -1,28 +1,57 @@
 package main
 
 import (
+    "bufio"
     "fmt"
+    "os"
+    "strconv"
+    "strings"
+	"time"
 )
 
-func main(){
+func main() {
     var numberOfPlayers int
     var maxValueForExtractedNumber int
 
     fmt.Println("Welcome to Guess the Number")
 
+	//obtaining the user values
     fmt.Print("Enter the number of players: ")
-	scanInteger(&numberOfPlayers)
+    numberOfPlayers = takeIntegerInput()
 
-   	fmt.Print("Enter the max value for the extracted number: ")
-	scanInteger(&maxValueForExtractedNumber)
+    fmt.Print("Enter the max value for the extracted number: ")
+    maxValueForExtractedNumber = takeIntegerInput()
 
     fmt.Println("The number of players is :", numberOfPlayers)
-    fmt.Println("The range of the extacted number is: 0 -", maxValueForExtractedNumber)
+    fmt.Println("The range of the extracted number is: 0 -", maxValueForExtractedNumber)
+	
+	//starting the oracle goroutine
+	go Oracle()
+
+	//sleep added to execute the oracle routine before main terminates
+	time.Sleep(1 * time.Second)
+
 }
 
-func scanInteger(variable *int){
-    if _, err := fmt.Scanln(variable); err != nil {
-        fmt.Println("Invalid input. Please enter a valid integer.")
-        return
+func scanInteger(variable *int) bool {
+    reader := bufio.NewReader(os.Stdin)
+    input, _ := reader.ReadString('\n')
+    input = strings.TrimSpace(input)
+    value, err := strconv.Atoi(input)
+    if err != nil {
+        return false
     }
+    *variable = value
+    return true
+}
+
+func takeIntegerInput() int {
+    var result int
+    for {
+        if scanInteger(&result) {
+            break
+        }
+        fmt.Println("Invalid input. Please enter a valid integer.")
+    }
+    return result
 }
---
+++ Assignment-03/part3/src/oracle.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+    "fmt"
+)
+
+func Oracle() {
+    fmt.Println("Oracle function is running")
+}
