7cf36f6be6f91e29327b54a28bb6ee8470df8879
FilippoGurioli <filippo.gurioli@studio.unibo.it>
2024-06-29 09:40:55 +0200

    Added part1 project with akka dependecies

---
+++ Assignment-03/part1/src/app/src/main/java/simtrafficbase/entity/CarAgent.java
@@ -0,0 +1,75 @@
+package simtrafficbase.entity;
+
+import simengine.*;
+import simtrafficbase.environment.Road;
+
+/**
+ * 
+ * Base class modeling the skeleton of an agent modeling a car in the traffic environment
+ * 
+ */
+public class CarAgent extends AbstractAgent implements RoadEntity{
+	
+	/* car model */
+	protected double maxSpeed;
+	protected double currentSpeed;
+	protected double acceleration;
+	protected double deceleration;
+	protected double currentPosition;
+	protected Road road;
+
+	
+	public CarAgent(String id, Road road, 
+			double initialPos, 
+			double acc, 
+			double dec,
+			double vmax) {
+		super(id);
+		this.road = road;
+		this.acceleration = acc;
+		this.deceleration = dec;
+		this.maxSpeed = vmax;
+		this.currentSpeed = 0;
+		this.currentPosition = initialPos;
+	}
+
+	public double getCurrentSpeed() {
+		return currentSpeed;
+	}
+	
+	public void setCurrentSpeed(double currentSpeed) {
+		this.currentSpeed = currentSpeed;
+	}
+	
+	public double getAcceleration() {
+		return acceleration;
+	}
+	
+	public void setAcceleration(double acceleration) {
+		this.acceleration = acceleration;
+	}
+	
+	public double getDeceleration() {
+		return deceleration;
+	}
+	
+	public void setDeceleration(double deceleration) {
+		this.deceleration = deceleration;
+	}
+	
+	public double getCurrentPosition() {
+		return currentPosition;
+	}
+
+	public void setCurrentPosition(double currentPosition) {
+		this.currentPosition = currentPosition;
+	}
+	
+	public Road getRoad() {
+		return this.road;
+	}
+
+	public double getMaxSpeed(){
+		return this.maxSpeed;
+	}
+}
---
+++ Assignment-03/part1/src/app/src/main/java/simtrafficbase/entity/TrafficLight.java
@@ -0,0 +1,73 @@
+package simtrafficbase.entity;
+
+import simtrafficbase.environment.Road;
+
+public class TrafficLight implements RoadEntity{
+	public static enum TrafficLightState {GREEN, YELLOW, RED}
+	private TrafficLightState state;
+	private int currentTimeInState;
+	private int redDuration, greenDuration, yellowDuration;
+	private double position;
+    private Road road;
+	
+	public TrafficLight(double pos, Road road, TrafficLightState initialState, int greenDuration, int yellowDuration, int redDuration) {
+		this.redDuration = redDuration;
+		this.greenDuration = greenDuration;
+		this.yellowDuration = yellowDuration;
+		this.position = pos;
+        this.road = road;
+		state = initialState;
+		currentTimeInState = 0;
+	}
+
+	public void step(int dt) {
+		switch (state) {
+		case GREEN: 
+			currentTimeInState += dt;
+			if (currentTimeInState >= greenDuration) {
+				state = TrafficLightState.YELLOW; 
+				currentTimeInState = 0;
+			}
+			break;
+		case RED: 
+			currentTimeInState += dt;
+			if (currentTimeInState >= redDuration) {
+				state = TrafficLightState.GREEN; 
+				currentTimeInState = 0;
+			}
+			break;
+		case YELLOW: 
+			currentTimeInState += dt;
+			if (currentTimeInState >= yellowDuration) {
+				state = TrafficLightState.RED; 
+				currentTimeInState = 0;
+			}
+			break;
+		default:
+			break;
+		}
+	}
+	
+	public boolean isGreen() {
+		return state.equals(TrafficLightState.GREEN);
+	}
+	
+	public boolean isRed() {
+		return state.equals(TrafficLightState.RED);
+	}
+
+	public boolean isYellow() {
+		return state.equals(TrafficLightState.YELLOW);
+	}
+	
+    @Override
+	public double getCurrentPosition() {
+		return position;
+	}
+
+    @Override
+    public Road getRoad() {
+        return road;
+    }
+
+}
